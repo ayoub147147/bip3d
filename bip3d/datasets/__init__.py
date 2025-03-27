1a86100620355d0f06db88b8f345f5f650d51519
free <219957360+flobh70@users.noreply.github.com>
2025-03-27 19:39:26 +0800

    Update __init__.py

--- bip3d/datasets/__init__.py
+++ bip3d/datasets/__init__.py
@@ -1,6 +1,5 @@
 from .embodiedscan_det_grounding_dataset import EmbodiedScanDetGroundingDataset
 from .transforms import *  # noqa: F401,F403
-from .sampler import *
 
 __all__ = [
     'EmbodiedScanDetGroundingDataset'
